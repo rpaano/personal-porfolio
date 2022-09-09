51e401f39c7a0e2672bccd9f813acdec7e42f6cd
Romel Niño Paano <50399801+rpaano@users.noreply.github.com>
2022-09-09 19:40:27 +0800

    Add project page

--- ServicesContract.ts
+++ ServicesContract.ts
@@ -11,3 +11,15 @@ export interface SkillContract {
     level: string,
     Icon: IconType,
 }
+
+export interface Project {
+    name: string,
+    description: string,
+    image_path: string,
+    deployed_url: string,
+    github_url: string,
+    category: Category[],
+    key_techs: string[],
+}
+
+export type Category = "react" | "node" | "express" | "django" | "mongo";
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,13 +1,24 @@
 import React from 'react'
+import ProjectCard from '../components/ProjectCard'
+import { projects } from '../data'
 
-const projects = () => {
+const Projects = () => {
     
     
     return (
-        <div className="p-4">
-            Projects Page
+        <div className="px-5 py-2 overflow-y-scroll" style={{height: '64vh'}}>
+            <nav>Navbar</nav>
+            <div className="relative grid grid-cols-12 gap-4 my-3">
+                {
+                    projects.map(project => (
+                        <div className="col-span-12 p-2 bg-gray-200 rounded-lg sm:col-span-6 lg:col-span-4 dark:bg-dark-200">
+                            <ProjectCard project={ project } key={ project.name }/>
+                        </div>
+                    ))
+                }
+            </div>
         </div>
     )
 }
 
-export default projects
+export default Projects
