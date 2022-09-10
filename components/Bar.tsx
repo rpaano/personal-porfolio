e9b397a8dc0e03bd8706b230fa3335842d522877
Romel Niño Paano <50399801+rpaano@users.noreply.github.com>
2022-09-10 19:27:51 +0800

    Update bar

--- components/Bar.tsx
+++ components/Bar.tsx
@@ -2,16 +2,19 @@ import React, { FunctionComponent } from 'react'
 import { SkillContract } from '../ServicesContract'
 
 const Bar:FunctionComponent<{ data: SkillContract }> = ({ data : {Icon, name, level}}) => {
-  return (
-    <div className="my-2 text-white bg-gray-300 rounded-full dark:bg-dark-300">
-        <div className="flex items-center px-4 py-1 rounded-full bg-gradient-to-r from-green to-blue-600"
-            style={{ width: level }}    
-        >
-            <Icon className="mr-3"/>
-            { name }
+    
+    const barWidth = `${level}%`;
+    
+    return (
+        <div className="my-2 text-white bg-gray-300 rounded-full dark:bg-dark-300">
+            <div className="flex items-center px-4 py-1 rounded-full bg-gradient-to-r from-green to-blue-600"
+                style={{ width: barWidth }}
+            >
+                <Icon className="mr-3"/>
+                { name }
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Bar
--- data.ts
+++ data.ts
@@ -45,57 +45,57 @@ export const services:ServiceContract[] = [
 
 export const languages: SkillContract[] = [
     {
-      Icon: BsCircleFill,
-      name: "Python",
-      level: "45%",
+        Icon: BsCircleFill,
+        name: "Python",
+        level: "45",
     },
     {
-      Icon: BsCircleFill,
-      name: "Java Script",
-      level: "60%",
+        Icon: BsCircleFill,
+        name: "Java Script",
+        level: "60",
     },
     {
-      Icon: BsCircleFill,
-      name: "React Native",
-      level: "80%",
+        Icon: BsCircleFill,
+        name: "React Native",
+        level: "80",
     },
     {
-      Icon: BsCircleFill,
-      name: "React",
-      level: "70%",
+        Icon: BsCircleFill,
+        name: "React",
+        level: "70",
     },
     {
-      Icon: BsCircleFill,
-      name: "Django",
-      level: "80%",
+        Icon: BsCircleFill,
+        name: "Django",
+        level: "80",
     },
     {
-      Icon: BsCircleFill,
-      name: "Bootstrap",
-      level: "80%",
+        Icon: BsCircleFill,
+        name: "Bootstrap",
+        level: "80",
     },
   ];
   
 export const tools: SkillContract[] = [
     {
-      Icon: BsCircleFill,
-      name: "Figma",
-      level: "85%",
+        Icon: BsCircleFill,
+        name: "Figma",
+        level: "85",
     },
     {
-      Icon: BsCircleFill,
-      name: "Photoshop",
-      level: "45%",
+        Icon: BsCircleFill,
+        name: "Photoshop",
+        level: "45",
     },
     {
-      Icon: BsCircleFill,
-      name: "Illustrator",
-      level: "60%",
+        Icon: BsCircleFill,
+        name: "Illustrator",
+        level: "60",
     },
     {
-      Icon: BsCircleFill,
-      name: "Framer",
-      level: "45%",
+        Icon: BsCircleFill,
+        name: "Framer",
+        level: "45",
     },
 ];
 
